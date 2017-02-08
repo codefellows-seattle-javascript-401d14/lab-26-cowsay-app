b580eddba15ffa58283f6e34e2b8ab75860eee3a
tommytnez <tommytnez@yahoo.com>
2017-02-08 10:19:17 -0800

    lab-thomas

--- app/entry.js
+++ app/entry.js
@@ -9,6 +9,21 @@ angular.module('demoApp', [])
   controller: ['$log', function($log){
     this.$onInit = function(){
       this.title = 'I talk like a cow';
+      this.userInput = '';
+      this.history = [];
+      this.currentCow = '';
+
+      this.saveCow = function(){
+        this.history.push(this.getCow(this.userInput));
+        this.currentCow = this.history[this.history.length -1];
+
+      };
+      this.undoCow = function(){
+        this.history.pop();
+        this.currentCow = this.history[this.history.length -1];
+      };
+
+
       this.getCow = function(text){
         $log.log('test was', text);
         return cowsay.say({text: text || 'irvine says weird stuff'});
